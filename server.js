7a7a9c1334d141269db2b673515086691c2642a3
Nicholas <nickycrandall@gmail.com>
2021-04-15 19:48:07 -0400

    CORS setup w/ localhost whitelist

--- server.js
+++ server.js
@@ -2,9 +2,24 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const PORT = 3003
+const cors = require('cors')
 
 //MIDDLEWARE
 app.use(express.json())
+
+//Cors
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+	origin: (origin, callback) => {
+		if (whitelist.indexOf(origin) !== -1 || !origin) {
+			callback(null, true)
+		} else {
+			callback(new Error('Not allowed by CORS'))
+		}
+	}
+}
+
+app.use(cors(corsOptions))
 
 //mongoose
 mongoose.connect('mongodb://localhost:27017/trailblazerDB', {
